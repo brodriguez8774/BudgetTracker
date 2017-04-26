98b301f0b0cca14866f7b902864359cdfe951d79
Brandon Rodriguez <brodriguez8774@gmail.com>
2017-04-26 18:46:26 -0400

    Add transaction class and tests

---
+++ BudgetTracker/Transaction.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker
+{
+    public class Transaction
+    {
+        #region Variables
+
+        private string description;
+        private DateTime dateProcessed;
+        private DateTime dateDue;
+        private decimal transactionAmount;
+
+        #endregion Variables
+
+        #region Constructors
+
+        public Transaction(string description, DateTime dateProcessed, DateTime dateDue, decimal transactionAmount)
+        {
+            Description = description;
+            DateProcessed = dateProcessed;
+            DateDue = dateDue;
+            TransactionAmount = transactionAmount;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("description", "Invalid description. Value is null.");
+                }
+                if (value.Trim() == "")
+                {
+                    throw new ArgumentException("Invalid description. Value is empty.");
+                }
+                description = value;
+            }
+        }
+
+        public DateTime DateProcessed
+        {
+            get { return dateProcessed; }
+            set
+            {
+                if (value == DateTime.MinValue)
+                {
+                    throw new ArgumentException("dateProcessed", "Invalid date processed. Value is null.");
+                }
+                if (value.CompareTo(DateTime.Today) > 0)
+                {
+                    throw new ArgumentException("dateProcessed", "Invalide date processed. Value is in the future.");
+                }
+                dateProcessed = value;
+            }
+        }
+
+        public DateTime DateDue
+        {
+            get { return dateDue; }
+            set
+            {
+                if (value == DateTime.MinValue)
+                {
+                    throw new ArgumentException("dateDue", "Invalid date due. Value is null.");
+                }
+                dateDue = value;
+            }
+        }
+
+        public decimal TransactionAmount
+        {
+            get { return transactionAmount; }
+            set
+            { transactionAmount = value; }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+
+
+        #endregion Methods
+
+    }
+}
---
+++ BudgetTrackerTests/TransactionTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BudgetTracker;
+
+
+namespace BudgetTrackerTests
+{
+    [TestClass]
+    public class TransactionTests
+    {
+        #region Variables
+
+        DateTime dateToday = DateTime.Today;
+        DateTime dateTomorrow = DateTime.Today.AddDays(1);
+        DateTime dateYesterday = DateTime.Today.AddDays(-1);
+
+        private string description;
+        private string descriptionNull;
+        private string descriptionEmpty;
+        private DateTime dateProcessed;
+        private DateTime dateProcessedDefault;
+        private DateTime dateProcessedFuture;
+        private DateTime dateDue;
+        private DateTime dateDueDefault;
+        private decimal transactionAmount;
+
+        #endregion Variables
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            description = "Test Description";
+            descriptionEmpty = "";
+            dateProcessed = dateToday;
+            dateProcessedFuture = dateTomorrow;
+            dateDue = dateToday;
+            transactionAmount = 123.0m;
+        }
+
+         #region Model Creation Tests
+
+        [TestMethod]
+        public void Test_TransactionCreation_Good()
+        {
+            Transaction transaction = new Transaction(description, dateProcessed, dateDue, transactionAmount);
+            Assert.AreEqual(description, transaction.Description);
+            Assert.AreEqual(dateProcessed, transaction.DateProcessed);
+            Assert.AreEqual(dateDue, transaction.DateDue);
+            Assert.AreEqual(transactionAmount, transaction.TransactionAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Test_TransactionCreation_DescriptionNull()
+        {
+            Transaction transaction = new Transaction(descriptionNull, dateProcessed, dateDue, transactionAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_TransactionCreation_DescriptionEmpty()
+        {
+            Transaction transaction = new Transaction(descriptionEmpty, dateProcessed, dateDue, transactionAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_TransactionCreation_DateProcessedDefault()
+        {
+            Transaction transaction = new Transaction(description, dateProcessedDefault, dateDue, transactionAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_TransactionCreation_DateProcessedInFuture()
+        {
+            Transaction transaction = new Transaction(description, dateProcessedFuture, dateDue, transactionAmount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_TransactionCreation_DateDueDefault()
+        {
+            Transaction transaction = new Transaction(description, dateProcessed, dateDueDefault, transactionAmount);
+        }
+
+        #endregion Model Creation Tests
+
+    }
+}
