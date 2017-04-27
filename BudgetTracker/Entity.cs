a2ee626a234c2dac010ad132e8a35cdd47b7cbb0
Brandon Rodriguez <brodriguez8774@gmail.com>
2017-04-27 16:55:10 -0400

    Rename "person" class to "entity"
    Thus accounts for businesses, companies, etc

---
+++ BudgetTracker/Entity.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BudgetTracker
+{
+    public class Entity
+    {
+        #region Variables
+
+        protected Address address;              // Addess of entity.
+        protected string category;              // Type of entity (person, company, etc).
+        protected string firstName;             // "Main" name of entity.
+        protected string lastName;              // "Optional"/additonal name of entity. Leave blank for companies, etc.
+        protected long phoneNumber;             // Contact phone number.
+
+        #endregion Variables
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor for a person entity.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="address"></param>
+        /// <param name="phoneNumber"></param>
+        public Entity(string category, string firstName, string lastName, Address address, long phoneNumber)
+        {
+            Category = category;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
+        /// <summary>
+        /// Constructor for a company entity.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="address"></param>
+        /// <param name="phoneNumber"></param>
+        public Entity(string category, string firstName, Address address, long phoneNumber)
+        {
+            Category = category;
+            FirstName = firstName;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string Category
+        {
+            get { return category; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("category", "Invalid category. Value is null.");
+                }
+                if (value.Trim() == "")
+                {
+                    throw new ArgumentException("Invalid category. Value is empty.", "category");
+                }
+                category = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("firstName", "Invalid first name. Value is null.");
+                }
+                if (value.Trim() == "")
+                {
+                    throw new ArgumentException("Invalid first name. Value is empty.", "firstName");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("lastName", "Invalid last name. Value is null.");
+                }
+                if (value.Trim() == "")
+                {
+                    throw new ArgumentException("Invalid last name. Value is empty.", "lastName");
+                }
+                lastName = value;
+            }
+        }
+
+        public Address Address
+        {
+            get { return address; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("address", "Invalid address. Value is null.");
+                }
+                address = value;
+            }
+        }
+
+        public long PhoneNumber
+        {
+            get { return phoneNumber; }
+            set
+            {
+                if (value.ToString().Length < 10)
+                {
+                    throw new ArgumentOutOfRangeException("phoneNumber", "Invalid phone number. Value is less than 10 digits.");
+                }
+                if (value.ToString().Length > 10)
+                {
+                    throw new ArgumentOutOfRangeException("phoneNumber", "Invalid phone number. Value is more than 10 digits.");
+                }
+                phoneNumber = value;
+            }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+
+
+        #endregion Methods
+
+    }
+}
---
+++ BudgetTrackerTests/EntityTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BudgetTracker;
+
+
+namespace BudgetTrackerTests
+{
+    [TestClass]
+    public class EntityTests
+    {
+        #region Variables
+
+        Address address;
+        Address addressNull;
+
+        private string category;
+        private string categoryNull;
+        private string categoryEmpty;
+        private string firstName;
+        private string firstNameNull;
+        private string firstNameEmpty;
+        private string lastName;
+        private string lastNameNull;
+        private string lastNameEmpty;
+        private long phoneNumber;
+        private long phoneNumberSmall;
+        private long phoneNumberLarge;
+
+        #endregion Variables
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            address = new Address("Test Street", "Test City", "Test Region", 12345);
+
+            category = "Test Category";
+            categoryEmpty = "";
+            firstName = "Test First Name";
+            firstNameEmpty = "";
+            lastName = "Test Last Name";
+            lastNameEmpty = "";
+            phoneNumber = 1234567890;
+            phoneNumberSmall = 123456789;
+            phoneNumberLarge = 12345678901;
+        }
+
+        #region Model Creation Tests
+
+        [TestMethod]
+        public void Test_EntityCreation_Good()
+        {
+            Entity entityObject = new Entity(category, firstName, lastName, address, phoneNumber);
+            Assert.AreEqual(category, entityObject.Category);
+            Assert.AreEqual(firstName, entityObject.FirstName);
+            Assert.AreEqual(lastName, entityObject.LastName);
+            Assert.AreEqual(address, entityObject.Address);
+            Assert.AreEqual(phoneNumber, entityObject.PhoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Test_EntityCreation_CategoryNull()
+        {
+            Entity entityObject = new Entity(categoryNull, firstName, lastName, address, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_EntityCreation_CategoryEmpty()
+        {
+            Entity entityObject = new Entity(categoryEmpty, firstName, lastName, address, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Test_EntityCreation_FirstNameNull()
+        {
+            Entity entityObject = new Entity(category, firstNameNull, lastName, address, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_EntityCreation_FirstNameEmpty()
+        {
+            Entity entityObject = new Entity(category, firstNameEmpty, lastName, address, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Test_EntityCreation_LastNameNull()
+        {
+            Entity entityObject = new Entity(category, firstName, lastNameNull, address, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_EntityCreation_LastNameEmpty()
+        {
+            Entity entityObject = new Entity(category, firstName, lastNameEmpty, address, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Test_EntityCreation_AddressNull()
+        {
+            Entity entityObject = new Entity(category, firstName, lastName, addressNull, phoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_EntityCreation_PhoneNumberSmall()
+        {
+            Entity entityObject = new Entity(category, firstName, lastName, address, phoneNumberSmall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Test_EntityCreation_PhoneNumberLarge()
+        {
+            Entity entityObject = new Entity(category, firstName, lastName, address, phoneNumberLarge);
+        }
+
+        #endregion Model Creation Tests
+
+    }
+}
